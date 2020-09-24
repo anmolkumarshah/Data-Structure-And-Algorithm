093baa6f6c992ecfdb25ad18acb84a89a6fccc64
AnmolKumar Shah <anmolshah.20k@gmail.com>
2020-09-24 19:08:47 +0530

    'Arrays'

--- Array/main.cpp
+++ Array/main.cpp
@@ -3,6 +3,8 @@
 
 
 int main(){
+
+   // 2D ARRAY USING STATIC DECLARATION
     int A[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
@@ -10,6 +12,38 @@ int main(){
         }
         printf("\n");
     }
+    printf("\n\n\n");
+
+ // 2D ARRAY USING DYNAMIC DECLARATION
+
+ int *B[3];
+ B[0] = (int *)malloc(3*sizeof(int));
+ B[1] = (int *)malloc(3*sizeof(int));
+ B[2] = (int *)malloc(3*sizeof(int));
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d\t",B[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n\n");
+ // three arrays in the heap and one array of pointers in the stack pointing to respective array
+ // making three dimensional
+
+ int **C = (int **)malloc(3*sizeof(int *));
+ C[0] = (int *)malloc(3*sizeof(int));
+ C[1] = (int *)malloc(3*sizeof(int));
+ C[2] = (int *)malloc(3*sizeof(int));
+
+   for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d\t",C[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n\n");
+
 
 return 0;
 }
